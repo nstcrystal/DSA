10d9555d687690cb9f0b274281a2ced1db221f24
Duypheloi <nstcrystal@gmail.com>
2026-03-28 13:45:05 +0700

    Add: singly_linked_list_basic.cpp

---
+++ data_structures/linked_list/singly_linked_list_basic.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+};
+
+Node *createNode(int x) {
+    Node *node;
+    node = new Node();
+
+    node->data = x;
+    node->next = nullptr;
+
+    return node;
+}
+
+void insertFirst(Node *&head, int x) {
+    Node *node = createNode(x);
+
+    if (head != nullptr) {
+        node->next = head;
+    }
+    head = node;
+}
+
+void browse(Node *head) {
+    while (head != nullptr) {
+        if (head->next == nullptr) {
+            cout << head->data << endl;
+        } else {
+            cout << head->data << " -> ";
+        }
+        head = head->next;
+    }
+}
+
+int main() {
+    Node *head = nullptr;
+
+    for (int i = 1; i < 5; i++) {
+        insertFirst(head, i);
+    }
+
+    browse(head);
+}
